f9433ca734b8ef6e306272ffef57069b4a392f34
eolculnamo2 <rbertram8@gmail.com>
2017-12-31 02:46:48 -0600

    corrected nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ class NavBar extends React.Component{
     render(){
         return(
             <div className="nav-bar">
-                <span onClick ={this.props.returnToSearch}>Git Profile Viewer</span>
+                <p className="nav-title" onClick ={this.props.returnToSearch}>Git Profile Viewer</p>
                 </div>
         )
     }
